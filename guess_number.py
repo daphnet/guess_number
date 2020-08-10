d0125da1953683a0e956d1ea00f0e38e63b79a7c
daphne tang <daphne.ch.tang@gmail.com>
2020-08-11 00:20:21 +0800

    advanced version guees_number.py

--- guess_number.py
+++ guess_number.py
@@ -4,13 +4,17 @@ import random
 
 r = random.randint (1, 100)
 
+count = 0
 while True:
+    count += 1 #count = count + 1
     num = input ('Please enter a number between 1 to 100: ')
     num = int(num)
     if num == r:
         print ('Great, you got it!!')
+        print ('This is try', count)
         break
     elif num < r:
         print ('Your guess is smaller than the answer.')
     elif num > r:
         print ('Your guess is larger than the answer.')
+    print ('This is try', count)
